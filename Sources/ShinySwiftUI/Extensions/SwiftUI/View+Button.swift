671f7cde40f37ee76ac6c964b1c1c4535549f8e1
Ben Myers <benl.myers01@gmail.com>
2021-11-30 11:58:24 -0800

    👻 Custom shortcuts and shortcut modifier on hover

---
+++ Sources/ShinySwiftUI/Extensions/SwiftUI/View+Button.swift
@@ -0,0 +1,94 @@
+//
+//  Button.swift
+//  
+//
+//  Created by Ben Myers on 11/22/21.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, iOS 13.0, *)
+public extension View {
+  
+  // MARK: - Public Methods
+  
+  /**
+   Adds a keyboard shortcut to the button, and displays the shortcut icon above the button when on hover.
+   
+   - parameter shortcut: The keyboard shortcut.
+   - returns: A modified `Button` with a hover effect.
+   */
+  func shortcut(_ shortcut: KeyboardShortcut) -> some View {
+    var str: String = ""
+    var symbols: [String] = []
+    let modifiers = shortcut.modifiers
+    if modifiers.contains(.control) {
+      symbols.append("control")
+    }
+    if modifiers.contains(.option) {
+      symbols.append("alt")
+    }
+    if modifiers.contains(.shift) {
+      symbols.append("shift")
+    }
+    if modifiers.contains(.command) {
+      symbols.append("command")
+    }
+    str += String(shortcut.key.character).capitalized
+    if str == " " {
+      str = "Space"
+    }
+    if #available(macOS 12.0, *) {
+      if shortcut == .defaultAction {
+        symbols.append("return")
+        str = "Enter"
+      }
+    }
+    return HoverView(button: self.keyboardShortcut(shortcut), string: str, symbols: symbols)
+  }
+  
+  /**
+   Adds a keyboard shortcut to the button, and displays the shortcut icon above the button when on hover.
+   
+   - parameter key: The key equivalent to check for.
+   - parameter modifiers: The modifiers affecting the key equivalent.
+   - returns: A modified `Button` with a hover effect.
+   */
+  @ViewBuilder
+  func shortcut(_ key: KeyEquivalent, modifiers: EventModifiers = .command) -> some View {
+    let shortcut = KeyboardShortcut(key, modifiers: modifiers)
+    self
+      .shortcut(shortcut)
+  }
+}
+
+@available(macOS 11.0, iOS 13.0, *)
+private struct HoverView<T>: View where T: View {
+  
+  @State var hover: Bool = false
+  
+  var button: T
+  var string: String
+  var symbols: [String]
+  
+  var body: some View {
+    ZStack {
+      button
+        .onHover { over in
+          hover = over
+        }
+      HStack(spacing: .xs) {
+        ForEach(symbols, id: \.self) { Image(systemName: $0) }
+        Text(string)
+      }
+      .imageScale(.small)
+      .padding(.xs)
+      .padding(.horizontal, .xs)
+      .background(VisualEffectView(material: .toolTip))
+      .roundedBorder(Color.primary.opacity(0.2), cornerRadius: .s)
+      .opacity(hover ? .opaque : .invisible)
+      .offset(y: hover ? -35.0 : -25.0)
+      .slickAnimation(value: hover)
+    }
+  }
+}
---
+++ Sources/ShinySwiftUI/Views/VisualEffectView.swift
@@ -0,0 +1,79 @@
+//
+//  VisualEffectView.swift
+//  
+//
+//  Created by Ben Myers on 11/29/21.
+//
+
+import SwiftUI
+
+#if os(macOS)
+public struct VisualEffectView: NSViewRepresentable {
+  
+  // MARK: - Public Properties
+  
+  /// The material of the visual effect view.
+  public var material: NSVisualEffectView.Material
+  /// Whether the view blends within the window.
+  public var blendingMode: NSVisualEffectView.BlendingMode
+  
+  // MARK: - Initalizers
+  
+  public init(material: NSVisualEffectView.Material = .popover, blendingMode: NSVisualEffectView.BlendingMode = .withinWindow) {
+    self.material = material
+    self.blendingMode = blendingMode
+  }
+  
+  // MARK: - Overidden Methods
+  
+  public func makeNSView(context: Context) -> NSVisualEffectView {
+    let visualEffectView = NSVisualEffectView()
+    visualEffectView.material = material
+    visualEffectView.blendingMode = blendingMode
+    visualEffectView.state = NSVisualEffectView.State.active
+    return visualEffectView
+  }
+  
+  public func updateNSView(_ visualEffectView: NSVisualEffectView, context: Context) {
+    visualEffectView.material = material
+    visualEffectView.blendingMode = blendingMode
+  }
+}
+#endif
+
+#if os(iOS)
+import UIKit
+
+public struct VisualEffectView: UIViewRepresentable {
+  
+  // MARK: - Wrapped Properties
+  
+  /// The current color scheme.
+  @Environment(\.colorScheme) var colorScheme
+  
+  // MARK: - Public Properties
+  
+  /// The effect for the view.
+  public var effect: UIVisualEffect?
+  
+  // MARK: - Initalizers
+  
+  public init(effect: UIVisualEffect?) {
+    self.effect = effect
+  }
+  
+  // MARK: - Overidden Methods
+  
+  public func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+    UIVisualEffectView()
+  }
+  
+  public func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+    if effect == nil {
+      uiView.effect = UIBlurEffect(style: colorScheme == .dark ? .dark : .light)
+    } else {
+      uiView.effect = effect
+    }
+  }
+}
+#endif
