50a32d2faadb092de8b01b480211497a5ae669a5
Ben Myers <benl.myers01@gmail.com>
2022-02-19 13:53:35 -0800

    🧹 Optimizations related to shortcut tooltip display

--- Sources/ShinySwiftUI/ShinySwiftUI.swift
+++ Sources/ShinySwiftUI/ShinySwiftUI.swift
@@ -13,13 +13,4 @@ public struct ShinySwiftUI {
   
   /// The `UserDefaults` key for whether buttons with shortcuts should display the shortcut's tooltip.
   public static let shortcutTooltipDefaultsKey = "shortcut_tooltip_enable"
-  
-  // MARK: - Public Static Methods
-  
-  /**
-   Prepares the default values provided by the package.
-   */
-  public static func prepare() {
-    UserDefaults.standard.register(defaults: [shortcutTooltipDefaultsKey : true])
-  }
 }
