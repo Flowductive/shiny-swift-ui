c1e41eeec8801f7a9e33aae692d4e470f238e803
Ben Myers <benl.myers01@gmail.com>
2022-02-26 14:59:13 -0800

    💡 Tooltip display modifier

--- Package.swift
+++ Package.swift
@@ -9,21 +9,19 @@ let package = Package(
       .iOS(.v13), .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShinySwiftUI",
             targets: ["ShinySwiftUI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/SwiftUIX/SwiftUIX", from: "0.1.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ShinySwiftUI",
-            dependencies: []),
+            dependencies: [
+              .product(name: "SwiftUIX", package: "SwiftUIX")
+            ]),
         .testTarget(
             name: "ShinySwiftUITests",
             dependencies: ["ShinySwiftUI"]),
---
+++ Sources/ShinySwiftUI/Extensions/SwiftUI/View/View+Tooltip.swift
@@ -0,0 +1,75 @@
+//
+//  View+Tooltip.swift
+//  
+//
+//  Created by Ben Myers on 2/21/22.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, iOS 13.0, *)
+public extension View {
+  
+  // MARK: - Public Methods
+  
+  /**
+   Adds a tooltip to the view.
+   
+   - parameter content: The content to display within the tooltip.
+   */
+  @ViewBuilder
+  func withTooltip<V>(present: Binding<Bool>? = nil, extraOffset: CGFloat = 0.0, content: () -> V) -> some View where V: View {
+    TooltipView(content: { self }, tooltipContent: content, hover: present, extraOffset: extraOffset)
+  }
+}
+
+@available(macOS 11.0, iOS 13.0, *)
+fileprivate struct TooltipView<Content, TooltipContent>: View where Content: View, TooltipContent: View {
+
+  @ViewBuilder var content: Content
+  @ViewBuilder var tooltipContent: TooltipContent
+  var hover: Binding<Bool>?
+  var extraOffset: CGFloat = 0.0
+  @State private var autoHover: Bool = false
+  @State private var contentHeight: CGFloat = 0.0
+  
+  var show: Bool {
+    if let hover = hover {
+      return hover.wrappedValue
+    } else {
+      return autoHover
+    }
+  }
+  
+  var body: some View {
+    GeometryReader { proxy in
+      content
+        .onAppear {
+          contentHeight = proxy.size.height
+        }
+    }
+    .onHover {
+      if hover == nil {
+        autoHover = $0
+      }
+    }
+    .overlay(
+      tooltipContent
+        .padding(.xs)
+        .background(background)
+        .cornerRadius(.s)
+        .roundedBorder(Color.primary.opacity(0.2), cornerRadius: .s)
+        .opacity(show ? .opaque : .invisible)
+        .offset(y: show ? -35.0 - extraOffset : -25.0)
+        .slickAnimation(value: show)
+    )
+  }
+  
+  var background: some View {
+    #if os(macOS)
+    return VisualEffectView(material: .toolTip)
+    #else
+    return VisualEffectView()
+    #endif
+  }
+}
