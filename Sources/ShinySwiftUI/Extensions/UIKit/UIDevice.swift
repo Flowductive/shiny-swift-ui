e8016681ba65a9dcb6f3ec6e55436acbd7effc6e
Ben Myers <benl.myers01@gmail.com>
2022-03-02 05:33:14 -0800

    📐 Bottom bar height computed property

--- Sources/ShinySwiftUI/Extensions/UIKit/UIDevice.swift
+++ Sources/ShinySwiftUI/Extensions/UIKit/UIDevice.swift
@@ -20,6 +20,15 @@ public extension UIDevice {
           return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
       }
   }
+  
+  /// Returns the size of the bottom bar (if on iPhone X or above).
+  static var bottomBarHeight: CGFloat {
+    if #available(iOS 13.0,  *) {
+        return abs(UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.safeAreaInsets.bottom ?? 0)
+    } else {
+        return abs(UIApplication.shared.delegate?.window??.safeAreaInsets.bottom ?? 0)
+    }
+  }
 }
 
 #endif
