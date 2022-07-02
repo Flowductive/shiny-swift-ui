769b6d1ccd75849b65ab05cf62e8c035bf23175b
Ben Myers <benl.myers01@gmail.com>
2022-07-02 00:55:44 -0400

    👒 Rundown improvements

--- Sources/ShinySwiftUI/Enumerations/_OpacityLevel.swift
+++ Sources/ShinySwiftUI/Enumerations/_OpacityLevel.swift
@@ -21,6 +21,9 @@ public enum _OpacityLevel: CGFloat {
   /// Quarter opaque.
   case quarter = 0.25
   
+  /// Slightly opaque.
+  case slight = 0.10
+  
   /// Almost invisible.
   case almostInvisible = 0.00001
   
