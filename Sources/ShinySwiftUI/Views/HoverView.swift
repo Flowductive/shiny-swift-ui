180f5102e9ff96d6215289f163ee9c61c4a188e7
Ben Myers <benl.myers01@gmail.com>
2022-08-10 19:08:52 -0700

    🔨 iOS version bug fixes

--- Sources/ShinySwiftUI/Views/HoverView.swift
+++ Sources/ShinySwiftUI/Views/HoverView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@available(macOS 11.0, *)
+@available(macOS 11.0, iOS 14.0, *)
 public struct HoverView<Content>: View where Content: View {
   
   // MARK: - Public Wrapped Properties
@@ -24,32 +24,59 @@ public struct HoverView<Content>: View where Content: View {
   
   /// The action to perform on click.
   public let action: () -> Void
+  /// The action to perform on hover.
+  public let onHover: (Bool) -> Void
 
   // MARK: - Public Body View
   
   public var body: some View {
     Button(action: action) {
-      content(hover, clicked).onHover { hover = $0 }
+      content(hover, clicked).onHover {
+        hover = $0
+        onHover($0)
+      }
     }
     .buttonStyle(HoverButtonStyle(pressed: $clicked))
   }
   
   // MARK: - Public Initalizers
   
-  public init(action: @escaping () -> Void = {}, content c: @escaping (Bool, Bool) -> Content) {
+  public init(action: @escaping () -> Void = {}, onHover: @escaping (Bool) -> Void = { _ in }, content c: @escaping (Bool, Bool) -> Content) {
     self.action = action
+    self.onHover = onHover
     self.content = c
   }
   
-  public init(action: @escaping () -> Void = {}, content c: @escaping (Bool) -> Content) {
+  public init(action: @escaping () -> Void = {}, onHover: @escaping (Bool) -> Void = { _ in }, content c: @escaping (Bool) -> Content) {
     self.action = action
+    self.onHover = onHover
     self.content = { (hover: Bool, click: Bool) in
       return c(hover)
     }
   }
+  
+  // MARK: - Public Methods
+  
+  /**
+   Animates the `hover` property of the view.
+   
+   - parameter animation: The Animation to use.
+   */
+  public func animateHover(_ animation: Animation = .slickEaseOut) -> some View {
+    return self.animation(animation, value: hover)
+  }
+  
+  /**
+   Animates the `clicked` property of the view.
+   
+   - parameter animation: The Animation to use.
+   */
+  public func animateClicked(_ animation: Animation = .slickEaseOut) -> some View {
+    return self.animation(animation, value: clicked)
+  }
 }
 
-@available(macOS 11.0, *)
+@available(macOS 11.0, iOS 14.0, *)
 fileprivate struct HoverButtonStyle: ButtonStyle {
   let pressed: Binding<Bool>
   func makeBody(configuration: Configuration) -> some View {
